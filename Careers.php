f8d9e89a6f343a56485f4220147cb0e9a8c69989
Harsh <harsh.thakkar.1511@gmail.com>
2019-10-25 02:32:45 -0400

    Careers Page Edited

--- Careers.php
+++ Careers.php
@@ -4,4 +4,17 @@
  * User: harsh
  * Date: 2019-10-25
  * Time: 2:22 AM
- */
+ */?>
+<html>
+<head>
+    <?php
+        include "header.php";
+    ?>
+</head>
+    <body>
+
+</body>
+<?php
+include "footer.php";
+?>
+</html>
